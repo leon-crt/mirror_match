fixed normalization to be linear and include minimums in position data

data_preprocessing.py
@@ -15,6 +15,8 @@ class MatchDataset(Dataset):
         max_posY = np.zeros((len(self.file_names)))
         max_stun = np.zeros((len(self.file_names)))
         max_meter = np.zeros((len(self.file_names)))
+        min_posX = np.zeros((len(self.file_names)))
+        min_posY = np.zeros((len(self.file_names)))
         len_seqs = np.zeros((len(self.file_names)))
         for i in range(len(self.file_names)):
             raw_data = pd.read_csv(self.dir_path + self.file_names[i])
@@ -23,8 +25,12 @@ class MatchDataset(Dataset):
             max_posY[i] = raw_data['PosY'].max()
             max_stun[i] = raw_data['Stun'].max()
             max_meter[i] = raw_data['Meter'].max()
+            min_posY[i] = raw_data['PosY'].min()
+            min_posX[i] = raw_data['PosX'].min()
         self.max_posX = max_posX.max()
+        self.min_posX = min_posY.min()
         self.max_posY = max_posY.max()
+        self.min_posY = min_posX.min()
         self.max_stun = max_stun.max()
         self.max_meter = max_meter.max()
         self.max_seq_len = int(len_seqs.max())
@@ -33,8 +39,8 @@ class MatchDataset(Dataset):
         return len(self.file_names)
 
     def norm_scalar_features(self, player_data):
-        player_data['PosX'] = player_data['PosX'] / self.max_posX
-        player_data['PosY'] = player_data['PosY'] / self.max_posY
+        player_data['PosX'] = player_data['PosX'] - self.min_posX / self.max_posX - self.min_posX
+        player_data['PosY'] = player_data['PosY'] - self.min_posY / self.max_posY - self.min_posY
         player_data['Health'] = player_data['Health'] / player_data['Health'].max()
         player_data['Meter'] = player_data['Meter'] / self.max_meter
         player_data['Stun'] = player_data['Stun'] / self.max_stun