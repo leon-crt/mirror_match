fixed bug in normalization step of dataloader

data_preprocessing.py
@@ -5,6 +5,11 @@ from torch.nn.utils.rnn import pad_sequence
 import pandas as pd
 import torch
 
+MAX_X = 928
+MIN_X = 93
+MAX_Y = 226
+MIN_Y = -42
+
 class MatchDataset(Dataset):
     def __init__(self, dir_path):
         self.dir_path = dir_path
@@ -28,9 +33,9 @@ class MatchDataset(Dataset):
             min_posY[i] = raw_data['PosY'].min()
             min_posX[i] = raw_data['PosX'].min()
         self.max_posX = max_posX.max()
-        self.min_posX = min_posY.min()
+        self.min_posX = min_posX.min()
         self.max_posY = max_posY.max()
-        self.min_posY = min_posX.min()
+        self.min_posY = min_posY.min()
         self.max_stun = max_stun.max()
         self.max_meter = max_meter.max()
         self.max_seq_len = int(len_seqs.max())
@@ -39,8 +44,8 @@ class MatchDataset(Dataset):
         return len(self.file_names)
 
     def norm_scalar_features(self, player_data):
-        player_data['PosX'] = player_data['PosX'] - self.min_posX / self.max_posX - self.min_posX
-        player_data['PosY'] = player_data['PosY'] - self.min_posY / self.max_posY - self.min_posY
+        player_data['PosX'] = (player_data['PosX'] - MIN_X) / (MAX_X- MIN_X)
+        player_data['PosY'] = (player_data['PosY'] - MIN_Y) / (MAX_Y - MIN_Y)
         player_data['Health'] = player_data['Health'] / player_data['Health'].max()
         player_data['Meter'] = player_data['Meter'] / self.max_meter
         player_data['Stun'] = player_data['Stun'] / self.max_stun

model_game_interface.py
@@ -32,7 +32,7 @@ memory = torch.zeros(1, 1, hidden_size, device=device)
 
 # load model
 ch_path = 'checkpoints/checkpoint_final'
-checkpoint = torch.load(ch_path)
+checkpoint = torch.load(ch_path, map_location=device)
 match_lstm.load_state_dict(checkpoint['model_state_dict'])
 
 # test LSTM
@@ -45,10 +45,11 @@ emu_proc = subprocess.Popen(["./fbneo/fcadefbneo.exe", "sfiii3nr1", "./model_gam
 
 output_vec = []
 time_vec = []
+# even = False
 # establish tcp connection
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST,PORT))
-    s.settimeout(5)
+    s.settimeout(2)
     try:
         # game loop
         while True:
@@ -74,26 +75,27 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                         # send through the socket as a comma separated list of numbers
                         conn.send(bytes(format_pred(player_inputs) + '\r\n', "utf-8"))
                         time_vec.append(elab_time_end)
+                        print(format_pred(player_inputs))
                         # if even:
                         #     conn.send(bytes('0,0,0,0,0,0,0,1,0,0,0,1\r\n', "utf-8"))
                         # else:
                         #     conn.send(bytes('0,0,0,0,0,0,0,0,0,0,0,0\r\n', "utf-8"))
                         # even = not even
-                        print(bytes(format_pred(player_inputs) + '\r\n', "utf-8"))
+
                     except (ConnectionResetError, BrokenPipeError) as e:
                         # catch abrupt network cut
                         print(f"Connection with {addr} was interrupted abruptly: {e}")
                         break
-    except:
+    except(TimeoutError):
         time_vec = np.array(time_vec)
         print(f'max time: {time_vec.max()}' )
         print(f'min time: {time_vec.min()}' )
         print(f'avg time: {time_vec.mean()}' )
-        # output_vec = np.array(output_vec)
-        # for i in range(len(output_vec)):
-        #     plt.hist(output_vec[:,i])
-        #     plt.title(str(i))
-        #     plt.show()
+        output_vec = np.array(output_vec)
+        for i in range(len(output_vec)-1):
+            plt.hist(output_vec[:,i])
+            plt.title(str(i))
+            plt.show()
 
 
 